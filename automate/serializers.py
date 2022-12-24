9b74d5277b2181bbfc015d80f2659490fc962d80
kramstyles <kramstyles@outlook.com>
2022-12-24 01:48:29 +0100

    removed unnecessary requirements from serializer

--- automate/serializers.py
+++ automate/serializers.py
@@ -15,9 +15,7 @@ class ProjectSerializer(serializers.ModelSerializer):
         exclude = ["created_at", "updated_at"]
         lookup_field = "slug"
         extra_kwargs = {
-            "owner": {"read_only": True},
-            "primary_repo_type": {"required": True},
-            "secondary_repo_type": {"required": True},
+            "owner": {"read_only": True}
         }
 
     def get_user(self, obj):
