326b2745c5da183b6e545c82ce446286c9498a63
Timizuo <timizuoebideri@gmail.com>
2023-01-08 23:10:20 +0100

    Add api test case

--- automate/serializers.py
+++ automate/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from rest_framework.reverse import reverse
 
+from accounts.serializers import UserSerializer
 from .models import Project
 from .utils import add_hook_to_repo
 
@@ -8,34 +9,31 @@ from .utils import add_hook_to_repo
 class ProjectSerializer(serializers.ModelSerializer):
     """Repository Serializer."""
 
-    user = serializers.SerializerMethodField()
+    owner = UserSerializer(read_only=True)
 
     class Meta:
         """Metaclass for Repository Serializer."""
 
         model = Project
+        fields = "__all__"
         lookup_field = "slug"
         extra_kwargs = {"owner": {"read_only": True}}
 
     def create(self, validated_data):
-        project = super().create(**validated_data)
+        project = super().create(validated_data)
 
         domain = self.context["request"].domain
         path = reverse("project:project-webhook", args=(project.slug,))
         project_webhook_url = domain + path
-
+        # TODO: Move this logic to celery
         add_hook_to_repo(
             project_webhook_url=project_webhook_url,
-            webhook_url=self.instance.primary_repo_webhook_url,
-            repo_type=self.instance.primary_repo_type,
-            repo_token=self.instance.primary_repo_token,
+            webhook_url=project.primary_repo_webhook_url,
+            repo_type=project.primary_repo_type,
+            repo_token=project.primary_repo_token,
         )
+        print(add_hook_to_repo)
         return project
-
-    def get_user(self, obj):
-        """Gets the user information and returns the name and email address."""
-        user = {"username": obj.owner.username, "email": obj.owner.email}
-        return user
 
     def validate(self, attrs):
         owner = self.context.get("owner")
