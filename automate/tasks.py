35013dff1bf9c16b100e2afbd67d5c0b7015401f
kramstyles <kramstyles@outlook.com>
2023-01-23 22:19:04 +0100

    tried ma best to make comments work

--- automate/tasks.py
+++ automate/tasks.py
@@ -25,6 +25,23 @@ def init_run_git(project, data):
     git.run()
 
 
+def bitbucket_refresh_access_token(project):
+    """This function was created to refresh the access token.
+    :param
+        project (Project): an instance where credentials would be fetched
+    :return
+        token (str): The newly refreshed token
+    """
+    credentials = {
+        "refresh_token": project.secondary_refresh_token,
+        "client_id": project.secondary_client_id,
+        "client_secret": project.secondary_client_secret,
+    }
+    credentials = crypt.multi_decrypt(credentials)
+    token = refresh_bitbucket_token(credentials)
+    return token
+
+
 @shared_task()
 def check_new_comments():
     """I'd get all Open PRs that have not been closed."""
@@ -35,8 +52,12 @@ def check_new_comments():
 
         for pr in open_pr:
             # Setup headers
-            primary_header = {"Authorization": f"Bearer {crypt.decrypt(pr.project.primary_repo_token)}"}
-            secondary_header = {"Authorization": f"Bearer {crypt.decrypt(pr.project.secondary_repo_token)}"}
+            primary_header = {
+                "Authorization": f"Bearer {crypt.decrypt(pr.project.primary_repo_token)}"
+            }
+            secondary_header = {
+                "Authorization": f"Bearer {crypt.decrypt(pr.project.secondary_repo_token)}"
+            }
 
             # Urls
             primary_url = pr.primary_url
@@ -47,6 +68,7 @@ def check_new_comments():
             sec_req = MakeRequest(secondary_url, secondary_header)
 
             owner = pr.project.owner
+            project = pr.project
 
             # Check if the secondary PR is github or bitbucket for merging operations
             if pr.project.secondary_repo_type == RepoTypeChoices.GITHUB.value:
@@ -61,19 +83,36 @@ def check_new_comments():
                                         ({secondary_url})""",
                     }
                     response = pri_req.put(data, json=True, url=pri_req.url + "/merge")
+                    status_code = response.status_code
+                    if status_code == 200 or status_code == 201:
+                        # Update Open PR History
+                        pr.action = "merged"
+                        pr.save()
 
-                    # Todo: get status code and break out
-                    break  # steps out of the loop
+                        activity = f"""`{owner}`; {pr.project.primary_repo_name} was automatically merged with
+                                    {pr.project.secondary_repo_name}. Passed with response `{status_code}`"""
+                        log_activity(
+                            user=owner,
+                            activity=activity,
+                            project=pr.project,
+                            status=True,
+                        )
+                    else:
+                        content = response.json()
+                        activity = f"""`{owner}`; {pr.project.primary_repo_name} couldn't merged with
+                                    {pr.project.secondary_repo_name}. Failed with response `{status_code}`."""
+                        log_activity(
+                            user=owner,
+                            activity=activity,
+                            project=pr.project,
+                            status=False,
+                        )
+                    break
             else:
                 # Refresh Token
-                credentials = {
-                    "refresh_token": pr.project.secondary_refresh_token,
-                    "client_id": pr.project.secondary_client_id,
-                    "client_secret": pr.project.secondary_client_secret
-                }
-                credentials = crypt.multi_decrypt(credentials)
-                new_token = refresh_bitbucket_token(credentials)
+                new_token = bitbucket_refresh_access_token(project)
                 new_header = {"Authorization": f"Bearer {new_token}"}
+
                 # checks if secondary PR is merged
                 req = MakeRequest(secondary_url, new_header)
                 response = req.get()
@@ -95,12 +134,25 @@ def check_new_comments():
                         pr.merged_at = content.get("updated_on")
                         pr.save()
 
-                        activity = f"`{owner}`; {pr.project.primary_repo_name} was automatically merged with {pr.project.secondary_repo_name}. Passed with response `{status_code}`"
-                        log_activity(user=owner, activity=activity, project=pr.project, status=True)
+                        activity = f"""`{owner}`; {pr.project.primary_repo_name} was automatically merged with
+                                    {pr.project.secondary_repo_name}. Passed with response `{status_code}`"""
+                        log_activity(
+                            user=owner,
+                            activity=activity,
+                            project=pr.project,
+                            status=True,
+                        )
                     else:
                         content = response.json()
-                        activity = f"`{owner}`; {pr.project.primary_repo_name} couldn't merged with {pr.project.secondary_repo_name}. Failed with response `{status_code}`. Reason is :{content.get('message')}"
-                        log_activity(user=owner, activity=activity, project=pr.project, status=True)
+                        activity = f"""`{owner}`; {pr.project.primary_repo_name} couldn't merged with
+                                    {pr.project.secondary_repo_name}. Failed with response `{status_code}`.
+                                    Reason is :{content.get('message')}"""
+                        log_activity(
+                            user=owner,
+                            activity=activity,
+                            project=pr.project,
+                            status=False,
+                        )
                     break
 
             if pr.project.secondary_repo_type == RepoTypeChoices.GITHUB.value:
@@ -176,3 +228,87 @@ def check_new_comments():
                         except AttributeError as err:
                             logger.error(err)
                             logger.critical(response)
+            else:
+                # Get number of comments existing in both Github and Bitbucket repositories
+                primary_response = pri_req.get(pri_req.url + "/comments")
+                primary_comments = json.loads(primary_response.content)
+
+                new_token = bitbucket_refresh_access_token(project)
+                header = {"Authorization": f"Bearer {new_token}"}
+                sec_req = MakeRequest(pr.url + "/comments?pagelen=100", headers=header)
+
+                response = sec_req.get()
+                secondary_comments = json.loads(response.content)
+
+                # Get's comments that are in the secondary that are not in the primary
+                comments_not_in_primary = []
+                for comment in secondary_comments["values"]:
+                    # Reduced comparable comment body
+                    sub_comment = comment.get("content")["html"]
+                    commit_id = comment.get("id")
+                    # Run a loop going through all comments in the primary comments and ensuring they are the same
+                    counter = 0
+
+                    if not primary_comments:
+                        # If there are no comments existing in the primary repository,
+                        # Add all secondary comments to the repository
+                        comments_not_in_primary.append({
+                            "body": sub_comment,
+                            "commit_id": commit_id
+                        })
+                    else:
+                        for pri_comment in primary_comments:
+                            primary_sub_comment = pri_comment.get("body")
+
+                            #  Compares the sub_comment with the primary comment. If they are the same,
+                            #  it increments the counter. If at the end of the loop there is no counter,
+                            #  it means the comment doesn't exist and it should be added
+
+                            if sub_comment == primary_sub_comment:
+                                counter += 1
+                                break
+
+                        if not counter:
+                            comments_not_in_primary.append({
+                                "body": sub_comment,
+                                "commit_id": commit_id
+                            })
+
+                # Next we check if the length of the content (comments) matches the comment field
+                if comments_not_in_primary:
+                    # Update the primary PR with comments
+                    for data in comments_not_in_primary:
+                        try:
+                            response = pri_req.post(url=pri_req.url + "/comments", data=data, json=True)
+                            status = response.status_code
+                            content = response.json()
+                            if status == 201:
+
+                                pr.comments = len(comments_not_in_primary)
+                                pr.save()
+                                activity = f"""`{owner}`; {pr.project.primary_repo_name} was automatically merged with
+                                            {pr.project.secondary_repo_name}. Passed with response `{status}`"""
+                                log_activity(
+                                    user=owner,
+                                    activity=activity,
+                                    project=pr.project,
+                                    status=True,
+                                )
+                            else:
+                                activity = f"""`{owner}`; {pr.project.primary_repo_name} wasn't merged with
+                                            {pr.project.secondary_repo_name}. Failed with response `{status}`"""
+                                log_activity(
+                                    user=owner,
+                                    activity=activity,
+                                    project=pr.project,
+                                    status=False,
+                                )
+                        except AttributeError as err:
+                            logger.error(err)
+                            logger.critical(response)
+                            log_activity(
+                                user=owner,
+                                activity=err,
+                                project=pr.project,
+                                status=True,
+                            )
