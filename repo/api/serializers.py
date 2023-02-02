fa3103062472093dacb14e1617afb0d15bacb28b
kramstyles <kramstyles@outlook.com>
2023-02-02 08:38:45 +0100

    created register endpoint

--- repo/api/serializers.py
+++ repo/api/serializers.py
@@ -1,5 +1,8 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
+
+User = get_user_model()
 
 
 class AuthTokenSerializer(serializers.Serializer):
@@ -14,3 +17,42 @@ class AuthTokenSerializer(serializers.Serializer):
             raise serializers.ValidationError("Invalid login credentials.")
 
         return {"user": user}
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    """Serializer to register new users."""
+
+    terms = serializers.BooleanField()
+
+    class Meta:
+        """Meta class for Register Serializer."""
+        model = User
+        exclude = ["created_at", "updated_at"]
+
+    def validate(self, attrs):
+        errors = []
+
+        validate_password(attrs["password"])
+
+        if not attrs.get("terms"):
+            errors.append("Read and accept our terms and conditions!")
+        if User.objects.filter(email=attrs.get("email")):
+            errors.append("Email address already in use.")
+
+        if errors:
+            raise serializers.ValidationError(errors)
+        return super().validate(attrs)
+
+
+    def create(self, validated_data):
+        """Create user with the object manager."""
+        data = validated_data
+        user = User.objects.create_user(
+            data["email"], data["password"], name=data["name"])
+
+        return data
+
+    def to_representation(self, instance):
+        data = {"message": "User created successfully!"}
+        return data
+
--- repo/api/urls.py
+++ repo/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from knox.views import LogoutAllView, LogoutView
 
-from repo.api.views import RepoLoginView
+from repo.api.views import RepoLoginView, RepoRegisterView
 
 urlpatterns = [
     path("auth/login/", RepoLoginView.as_view(), name="login"),
     path("auth/logout/", LogoutView.as_view(), name="logout"),
     path("auth/logoutall/", LogoutAllView.as_view(), name="logoutall"),
+    path("auth/register/", RepoRegisterView.as_view(), name="register"),
 ]
